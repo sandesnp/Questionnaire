a9b20f9883ee0a43bd1672594694c109c14e1c97
sandesnp <averagenp@gmail.com>
2021-01-06 17:23:06 +0545

    Made some changes on Appearance.

--- app/src/main/java/com/example/questionnaire/apiInterface/httpRequests.java
+++ app/src/main/java/com/example/questionnaire/apiInterface/httpRequests.java
@@ -7,12 +7,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface httpRequests {
-
-
     @GET("/api/set")
     Call<set> getSet();
 
     @GET("/api/faq")
     Call<faq> getFaq();
-
 }
